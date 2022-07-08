b85b90f7378197fdb698a7e3c36d3888eccf2388
unknown <alan-abdlky@hotmail.com>
2022-07-08 20:18:20 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('client',[\App\Http\Controllers\ClientController::class,'store']);
